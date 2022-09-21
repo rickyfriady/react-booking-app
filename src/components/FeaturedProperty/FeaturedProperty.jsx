139e34bdceb04fafe10c999a392353d8a811edfd
AllukyyDev <73653891+rickyfriady@users.noreply.github.com>
2022-09-21 17:29:29 +0700

    Update Component dan page 1. Update component featuredPropperty 2. update fonsize on header components 3. Update background color and padding-top on home css page

--- src/components/FeaturedProperty/FeaturedProperty.jsx
+++ src/components/FeaturedProperty/FeaturedProperty.jsx
@@ -3,13 +3,35 @@ import './featuredproperty.css'
 const FeaturedProperty = () => {
   return (
     <div className='fp'>
-      <img src="http://source.unsplash.com/500x500?spanyol" alt="img-property" className='fp-img' />
-      <span className='fp-name'>Aparthotel Miasto</span>
-      <span className='fp-city'>Madrid</span>
-      <span className='fp-price'>Starting price $120</span>
-      <div className="fp-rating">
-        <button>8.9</button>
-        <span>excelent</span>
+      <div className="fp-item">
+        <img src="http://source.unsplash.com/500x500" alt="img-property" className='fp-img' />
+        <span className='fp-name'>Aparthotel Miasto</span>
+        <span className='fp-city'>Madrid</span>
+        <span className='fp-price'>Starting price $120</span>
+        <div className="fp-rating">
+          <button>8.9</button>
+          <span>excelent</span>
+        </div>
+      </div>
+      <div className="fp-item">
+        <img src="http://source.unsplash.com/500x500" alt="img-property" className='fp-img' />
+        <span className='fp-name'>Aparthotel Miasto</span>
+        <span className='fp-city'>Madrid</span>
+        <span className='fp-price'>Starting price $120</span>
+        <div className="fp-rating">
+          <button>8.9</button>
+          <span>excelent</span>
+        </div>
+      </div>
+      <div className="fp-item">
+        <img src="http://source.unsplash.com/500x500" alt="img-property" className='fp-img' />
+        <span className='fp-name'>Aparthotel Miasto</span>
+        <span className='fp-city'>Madrid</span>
+        <span className='fp-price'>Starting price $120</span>
+        <div className="fp-rating">
+          <button>8.9</button>
+          <span>excelent</span>
+        </div>
       </div>
     </div>
   )
